4abd9d5aa13598ffbf19bb2dcd879997ddad6634
bloeys <bloeys@outlook.com>
2022-06-27 05:16:53 +0400

    Start

--- nmap.go
+++ nmap.go
@@ -1,4 +1,14 @@
 package nmap
 
-type NMap struct {
+type AllowedKeysIf interface {
+	uint8 | uint16 | uint32 | uint64 | uint
+}
+
+type element[T AllowedKeysIf, V any] struct {
+	Key   T
+	Value V
+}
+
+type NMap[T AllowedKeysIf, V any] struct {
+	Elements []element[T, V]
 }
